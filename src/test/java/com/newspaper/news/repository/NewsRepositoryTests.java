e86ccf6b8e8877bd190ec6d4a2dc7e9670bf1e85
Gabriel <gabrielsalves2k18@gmail.com>
2022-08-24 19:04:29 -0300

    Testes

--- src/test/java/com/newspaper/news/repository/NewsRepositoryTests.java
+++ src/test/java/com/newspaper/news/repository/NewsRepositoryTests.java
@@ -1,8 +1,46 @@
 package com.newspaper.news.repository;
 
+import com.newspaper.news.factory.CategoryFactory;
+import com.newspaper.news.factory.NewsFactory;
+import com.newspaper.news.model.Category;
+import com.newspaper.news.model.News;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 @DataJpaTest
 public class NewsRepositoryTests {
 
+    @Autowired
+    private NewsRepository newsRepository;
+
+    @Test
+    public void shouldFindByIdWhenCategoryExists() {
+        long id = 1L;
+        String title = "Empregos em alta";
+        News news = newsRepository.findById(id).get();
+
+        Assertions.assertEquals(title, news.getTitle());
+    }
+
+    @Test
+    public void shouldFindAllCategoryWhenExists() {
+        List<News> list = newsRepository.findAll();
+        String title1 = "Empregos em alta";
+        String title2 = "Guarulhos no progresso";
+
+        Assertions.assertEquals(title1, list.get(0).getTitle());
+        Assertions.assertEquals(title2, list.get(1).getTitle());
+    }
+
+    @Test
+    public void shouldCreateCategory() {
+        News news = NewsFactory.createNews();
+        news = newsRepository.save(news);
+
+        Assertions.assertEquals("Guarulhos", news.getTitle());
+    }
 }
---
+++ src/test/java/com/newspaper/news/repository/UserRepositoryTests.java
@@ -0,0 +1,45 @@
+package com.newspaper.news.repository;
+
+import com.newspaper.news.factory.CategoryFactory;
+import com.newspaper.news.factory.UserFactory;
+import com.newspaper.news.model.Category;
+import com.newspaper.news.model.Users;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest
+public class UserRepositoryTests {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void shouldFindByIdWhenCategoryExists() {
+        long id = 1L;
+        String userName = "Gabriel";
+        Users user = userRepository.findById(id).get();
+
+        Assertions.assertEquals(userName, user.getName());
+    }
+
+    @Test
+    public void shouldFindAllCategoryWhenExists() {
+        List<Users> list = userRepository.findAll();
+        String userName1 = "Gabriel";
+        String userName2 = "Teste";
+
+        Assertions.assertEquals(userName1, list.get(0).getName());
+        Assertions.assertEquals(userName2, list.get(1).getName());
+    }
+
+    @Test
+    public void shouldCreateCategory() {
+        Users user = UserFactory.createUser();
+        user = userRepository.save(user);
+
+        Assertions.assertEquals("Gabriel", user.getName());
+    }
+}
